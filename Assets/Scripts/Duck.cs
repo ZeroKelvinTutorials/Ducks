6bd8caa4e6435a3a8b2faf4e6671c53bd0cc0499
bernardolv <zerokelvintutorials@gmail.com>
2022-12-23 15:25:57 -0600

    _follower -> _follow

--- Assets/Scripts/Duck.cs
+++ Assets/Scripts/Duck.cs
@@ -6,7 +6,7 @@ public class Duck : MonoBehaviour
 {
     //can be either TransformFollower or MouseFollower
     //which are classes that implement the Follower abstract class
-    private Follow _follower;
+    private Follow _follow;
 
     public virtual void OnEnable()
     {
@@ -17,26 +17,26 @@ public class Duck : MonoBehaviour
         DuckManager.Ducks.Remove(this);
     }
 
-    //initializes _follower with new TransformFollower class
+    //initializes _follow with new TransformFollower class
     public void StartFollowingTransform(Transform followTransform, float distance, float speed)
     {
-        _follower = new FollowTransform(this.transform, followTransform);
-        _follower.StartFollowing(distance, speed);
+        _follow = new FollowTransform(this.transform, followTransform);
+        _follow.StartFollowing(distance, speed);
     }
 
-    //Initializes _follower with new MouseFollower class
+    //Initializes _follow with new MouseFollower class
     public void StartFollowingMouse(float distance, float speed)
     {
-        _follower = new FollowMouse(this.transform);
-        _follower.StartFollowing(distance, speed);
+        _follow = new FollowMouse(this.transform);
+        _follow.StartFollowing(distance, speed);
     }
 
     void Update()
     {
-        if (_follower != null && _follower.IsFollowing)
+        if (_follow != null && _follow.IsFollowing)
         {
-            transform.position = _follower.GetNextPosition();
-            transform.rotation = _follower.GetNextRotation();
+            transform.position = _follow.GetNextPosition();
+            transform.rotation = _follow.GetNextRotation();
         }
         TryQuack();
     }
